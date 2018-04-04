6b7e2c880889f96db52d4b0474177d6d2dbe4c4c
JeffMinsungKim <jeffminsungkim@gmail.com>
2018-04-04 09:50:54 +0900

    regex shorthands

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = n => {
-	const format = /^([a-zA-Z]+[0-9])[0-9]*$/;
+	const format = /^([a-zA-Z]+\d)\d*$/;
 
 	if (Object.prototype.toString.call(n) === '[object String]') {
 		return format.test(n);
